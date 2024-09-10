fb4a6d387a64014f75f498a5c7a53f08a1249fa3
Edwin Kiuma <62498603+edwin659898@users.noreply.github.com>
2024-09-10 13:15:14 +0300

    bootstrap

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d8157cfacaa695be87e806a233099b6bcd2e5f9ef06c910cf49a6bf010537ef7
-size 205
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $guarded=[];
+}
+
